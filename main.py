f2412b48319bc12f4fad4909a8ac2e61a1e113a2
RobertHan96 <yshan4329@gmail.com>
2020-09-17 16:17:55 +0900

    그래프 시각화 가독성 개선 : 데이터값  표시, 표 제목 추가 등

--- main.py
+++ main.py
@@ -41,8 +41,13 @@ class ChartsMaker :
         plt.title('가장 많이 언급된 단어')
         plt.xlabel('단어')
         plt.ylabel('언급 횟수')
-
-        plt.plot(words, mentioned_time, 'skyblue')
+        plt.plot(words, mentioned_time, 'skyblue', marker='o', ms=15, mfc='r')
+        for x, y in zip(words, mentioned_time):
+            label = y
+            plt.annotate(label,  # this is the text
+                         (x, y),  # this is the point to label
+                         va='center',
+                         ha='center')  # horizontal alignment can be left, right or center
         plt.show()
 
     @staticmethod
@@ -51,10 +56,7 @@ class ChartsMaker :
         plt.title('평점 추이')
         plt.xlabel('평점')
         plt.ylabel('평점 수')
-        plt.bar(ratings, rating_counts)
-        # for i, v in enumerate(rating_counts) :
-        #     string_value = '{}명'.format(rating_counts[i])
-        #     plt.text(v, rating_counts[i],string_value, fontsize=10, color='ff0000')
+        plt.bar(ratings, rating_counts, color='lightblue')
         plt.show()
 
     @staticmethod
@@ -63,6 +65,14 @@ class ChartsMaker :
         label = ['긍정', '부정']
         color = ['#14CCC0', '#FF1C6A']
         plt.pie(feedbacks, labels=label, colors=color, autopct='%1.f%%')
+        plt.axis('equal')
+        plt.show()
+
+    @staticmethod
+    def ratingPieChart(rating_count, labels_arr):
+        labels = ['{}점'.format(i) for i in labels_arr]
+        plt.title('평점 비율')
+        plt.pie(rating_count, labels=labels, autopct='%1.f%%')
         plt.axis('equal')
         plt.show()
 
@@ -107,6 +117,7 @@ def _main() :
 
     charts_maker.wordsFrequencyChart(nouns, nouns_count )
     charts_maker.ratingChart(ratings, rating_count)
+    charts_maker.ratingPieChart(rating_count, ratings)
     makeWordCloud(nouns_counter_dict)
 
 if __name__ == "__main__":
